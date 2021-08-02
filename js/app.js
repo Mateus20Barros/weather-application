8a8825a29e0cb73724c2d1ae8a329de1b9c4d212
Mateus Barros <matteusmaciel40@gmail.com>
2021-08-02 14:23:30 -0300

    :rocket: It's a JAVASCRIPT file folder.

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const body = document.body
+const form = document.querySelector('[data-js="form"]')
+const flipCard = document.querySelector('[data-js="card"]')
+const card = document.querySelector('[data-js="card_inner"]')
+const cityCard = document.querySelector('[data-js="city_card"]')
+const cityNameContainer = document.querySelector('[data-js="city_name"]')
+const timeIconContainer = document.querySelector('[data-js="time_icon"]')
+const changeThemeButton = document.querySelector('[data-js="change_theme"]')
+const cityWeatherContainer = document.querySelector('[data-js="city_weather"]')
+const weatherCitiesContainer = document.querySelector('[data-js="weather_container"]')
+const cityTemperatureContainer = document.querySelector('[data-js="city_temperature"]')
+
+const handleThemeChange = () => {
+    if (body.classList.contains('body')) {
+        body.classList.toggle('change_theme')
+    }
+}
+
+const isDayOrNight = IsDayTime => {
+    if (IsDayTime) {
+        cityCard.style.background = "url('./assets/src/day.jpg') no-repeat center"
+        cityCard.style.backgroundSize = "cover"
+        return
+    }
+    cityCard.style.background = "url('./assets/src/night.jpg') no-repeat"
+    cityCard.style.backgroundSize = "cover"
+}
+
+const getWeatherInfo = (timeIcon, LocalizedName, WeatherText, Temperature) => {
+    timeIconContainer.innerHTML = timeIcon
+    cityNameContainer.textContent = LocalizedName
+    cityWeatherContainer.textContent = WeatherText
+    cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
+
+const showCityWeatherInfo = async cityName => {
+    const [{ Key, LocalizedName }] = await getCityData(cityName)
+    const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
+    const timeIcon = `<img src="./assets/src/icons/${WeatherIcon}.svg" />`
+
+    getWeatherInfo(timeIcon, LocalizedName, WeatherText, Temperature)
+    isDayOrNight(IsDayTime)
+}
+
+const checkInputValue = inputValue => {
+    return inputValue === ''
+        ? flipCard.style.display = 'none'
+        : flipCard.style.display = 'block'
+}
+
+setInterval(() => card.classList.toggle('is-flipped'), 3500)
+
+const handleSubmit = event => {
+    event.preventDefault()
+    const inputValue = event.target.cityname.value
+    
+    checkInputValue(inputValue)
+    showCityWeatherInfo(inputValue)
+    form.reset()
+}
+
+changeThemeButton.addEventListener('click', handleThemeChange)
+form.addEventListener('submit', handleSubmit)
---
+++ js/weather_app.js
@@ -0,0 +1,33 @@
+const APIKey = 'G97v0yvl4TodaleQF3gjOc5dZymdGhV5'
+const baseUrl = 'http://dataservice.accuweather.com/'
+
+const getCityUrl = cityname => {
+    return `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityname}`
+}
+
+const getWeatherUrl = cityKey => {
+    return `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br`
+}
+
+const fetchData = async url => {
+    try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('Não foi possível obter os dados.')
+        }
+
+        return response.json()
+    }
+    catch({ name, message }) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+const getCityData = cityname => {
+    return fetchData(getCityUrl(cityname))
+}
+
+const getCityWeather = cityKey => {
+    return fetchData(getWeatherUrl(cityKey))
+}
